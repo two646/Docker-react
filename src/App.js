cdcd35e65a3b956c21001f39fa56e7a0819d7df4
Timothy Thao <timothythao5@gmail.com>
2024-04-03 21:23:28 -0700

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Welcome to the thunder zone
+           I am the feature branch
         </p>
         <a
           className="App-link"
