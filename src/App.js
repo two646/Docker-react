eb7d5fdd102e2fe9eb702c75bf5d3b273574239e
Timothy Thao <timothythao5@gmail.com>
2024-04-03 21:07:01 -0700

    thunder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           okay testing again?
+           Welcome to the thunder zone
         </p>
         <a
           className="App-link"
